2209fc37b079c9d13daa32f479e078603cb87ad2
Camiloo92 <casallas.ivan@gmail.com>
2025-02-12 20:56:43 -0500

    Se configura estrategia local

---
+++ backend/middlewares/passport/strategies/localStrategy.js
@@ -0,0 +1,32 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const pool = require("../../../libs/postgres");
+
+const localStrategy = new LocalStrategy(
+  {
+    usernameField: "username",
+    passwordField: "password",
+  },
+  async (username, password, done) => {
+    try {
+      const result = await pool.query(
+        "SELECT * FROM usuarios WHERE usu_nombre = $1",
+        [username]
+      );
+
+      if (result.rows.length === 0) {
+        return done(null, false, { message: "Usuario no encontrado" });
+      }
+      const user = result.rows[0];
+      const isMatch = await bcrypt.compare(password, usu_Hash);
+
+      if (!isMatch) {
+        return done(null, false, { message: "Contraseña incorrecta" });
+      }
+    } catch (error) {
+        return done(err)
+    }
+  }
+)
+
+module.exports= localStrategy
