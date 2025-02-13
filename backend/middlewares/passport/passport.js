9918312ef135891bc5543638058e68ba2417e1a3
Camiloo92 <casallas.ivan@gmail.com>
2025-02-12 20:57:32 -0500

    se configura passport con el servidor

---
+++ backend/middlewares/passport/passport.js
@@ -0,0 +1,7 @@
+const passport = require('passport')
+const LocalStrategy =require ('./strategies/localStrategy')
+
+
+passport.use(LocalStrategy)
+
+module.exports = passport
--- backend/models/server.model.js
+++ backend/models/server.model.js
@@ -1,46 +1,44 @@
-const express = require('express')
-const cors = require('cors')
-const { configServer }= require('../config/config')
-const router = require('../router/router')
-
+const express = require("express");
+const cors = require("cors");
+const { configServer } = require("../config/config");
+const router = require("../router/router");
+const passport = require("../middlewares/passport/passport");
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = configServer.port;
+    this.routerPath = "/api/v1";
+    this.middlewares();
+    this.routes();
+  }
 
-    constructor(){
-        this.app = express() 
-        this.port = configServer.port
-        this.routerPath = '/api/v1'
-        this.middlewares()
-        this.routes()
-    }
-    
-    /**
-     *Activa los Middlewares del servidor  
-     */
-    middlewares(){
-        this.app.use(express.json())
-        this.app.use(cors())
-    }
+  /**
+   *Activa los Middlewares del servidor
+   */
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use(cors());
+  }
 
-    /**
-     *Selecciona la ruta del App
-     */
-    routes(){
-        //http://localhost:3000/api/v1
-        this.app.use(this.routerPath, router)
-    }
+  /**
+   *Selecciona la ruta del App
+   */
+  routes() {
+    //http://localhost:3000/api/v1
+    this.app.use(this.routerPath, router);
+  }
 
-    /**
-     *Inicializa el servidor
-     */
-    init(){
-        this.app.listen(this.port, ()=>{
-            console.log(`Servidor iniciado en el puerto ${this.port}`);
-        })
-    }
+  /**
+   *Inicializa el servidor
+   */
+  init() {
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
+    this.app.listen(this.port, () => {
+      console.log(`Servidor iniciado en el puerto ${this.port}`);
+    });
+  }
 }
-    
 
-module.exports = Server
-
-
+module.exports = Server;
