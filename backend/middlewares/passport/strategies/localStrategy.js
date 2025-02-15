7636d498e908a6047950673d49fb8a40ea2b3590
Camiloo92 <casallas.ivan@gmail.com>
2025-02-14 20:23:54 -0500

    Se implementa la función de la base de datos

--- backend/middlewares/passport/strategies/localStrategy.js
+++ backend/middlewares/passport/strategies/localStrategy.js
@@ -5,27 +5,25 @@ const pool = require("../../../libs/postgres");
 /**
  * Estrategia local de inicio de sesión que autentica usuario con bd*/
 const localStrategy = new LocalStrategy(
-  {
-    usernameField: "username",
-    passwordField: "password",
-  },
   async (username, password, done) => {
     try {
       const result = await pool.query(
-        "SELECT * FROM usuarios WHERE usu_nombre = $1",
+        "SELECT * FROM obtener_usuario($1)",
         [username]
       );
+
       if (result.rows.length === 0) {
         return done(null, false, { message: "Usuario no encontrado" });
       }
       const user = result.rows[0];
-      const isMatch = await bcrypt.compare(password, usu_Hash);
+      const isMatch = await bcrypt.compare(password, user._usu_hash);
 
       if (!isMatch) {
         return done(null, false, { message: "Contraseña incorrecta" });
       }
+      done(null,user)
     } catch (error) {
-        return done(err)
+        return done(error)
     }
   }
 )
