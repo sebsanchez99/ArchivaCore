42b504dd16848051a4ab62ebc57d2052ecd0f013
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-02-16 09:45:57 -0500

    Creación de vista de prueba

---
+++ frontend/lib/prueba.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Prueba extends StatelessWidget {
+  const Prueba({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('inicio de sesión correcto'),
+    );
+  }
+}
