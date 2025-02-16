8661cb3f555d7e67c986eac4363111bdd7427523
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-02-16 09:43:01 -0500

    Generación de clase genérica para manejo de resultados

---
+++ frontend/lib/domain/either/either.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'either.freezed.dart';
+
+// Declaración de clase genérica que contiene dos posibles valores: L(Error), R(Éxito)
+@freezed
+class Either<L, R> with _$Either<L, R> {
+  factory Either.right(R value) = _Right;
+  factory Either.left(L value) = _Left;
+}
