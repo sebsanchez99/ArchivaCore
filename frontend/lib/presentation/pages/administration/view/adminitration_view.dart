95b148eb2902d97efc5b15a6c01dee0eb233f52f
Evafajardo <31.fajardo@gmail.com>
2025-03-15 12:01:00 -0500

    Se agrego estilos a la tabla de administrador

--- frontend/lib/presentation/pages/administration/view/adminitration_view.dart
+++ frontend/lib/presentation/pages/administration/view/adminitration_view.dart
@@ -44,39 +44,87 @@ class _AdminitrationViewState extends State<AdminitrationView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: SingleChildScrollView(
-        child: PaginatedDataTable(
-          header: Center(
-            child: SearchAnchor.bar(
-              searchController: _searchController,
-              suggestionsBuilder: (context, controller) {
-                final userName = controller.text.toLowerCase();
-                final results =
-                    users.where((user) {
-                      return user.name.toLowerCase().contains(userName);
-                    }).toList();
-                return results.map((user) {
-                  return ListTile(
-                    title: Text(user.name),
-                    onTap: () {
-                      controller.closeView(user.name);
-                      _searchController.text = user.name;
-                    },
-                  );
-                });
-              },
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            cardTheme: CardTheme(
+              elevation: 4,
+              surfaceTintColor: Colors.blueAccent,
             ),
           ),
+          child: PaginatedDataTable(
+            showFirstLastButtons: true,
+            arrowHeadColor: Colors.blue,
+            header: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  constraints: BoxConstraints(maxHeight: 40, maxWidth: 500),
+                  child: SearchAnchor.bar(
+                    viewConstraints: BoxConstraints(
+                      minHeight: kToolbarHeight,
+                      maxHeight: kToolbarHeight * 4,
+                    ),
+                    isFullScreen: false,
+                    searchController: _searchController,
+                    suggestionsBuilder: (context, controller) {
+                      final userName = controller.text.toLowerCase();
+                      final results =
+                          users.where((user) {
+                            return user.name.toLowerCase().contains(userName);
+                          }).toList();
+                      return results.map((user) {
+                        return SizedBox(
+                          child: ListTile(
+                            dense: true,
+                            title: Text(user.name),
+                            onTap: () {
+                              controller.closeView(user.name);
+                              _searchController.text = user.name;
+                            },
+                          ),
+                        );
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 185,
+                  height: 40,
+                  child: FilledButton.tonalIcon(
+                    onPressed: () {},
+                    label: Text('Agregar usuario'),
+                    icon: Icon(Icons.add),
+                    iconAlignment: IconAlignment.end,
+                    style: ButtonStyle(
+                      elevation: WidgetStatePropertyAll(3),
+                      iconSize: WidgetStatePropertyAll(30),
+                    ),
+                  ),
+                ),
+              ],
+            ),
 
-          rowsPerPage: 8,
-          columns: [
-            DataColumn(label: Text('Usuario')),
-            DataColumn(label: Text('Rol')),
-            DataColumn(label: Text('Acciones')),
-          ],
-          source: tableRow,
+            rowsPerPage: 8,
+            columns: [
+              DataColumn(
+                label: Text('Usuario'),
+                headingRowAlignment: MainAxisAlignment.center,
+              ),
+              DataColumn(
+                label: Text('Rol'),
+                headingRowAlignment: MainAxisAlignment.center,
+              ),
+              DataColumn(
+                label: Text('Acciones'),
+                headingRowAlignment: MainAxisAlignment.center,
+              ),
+            ],
+            source: tableRow,
+          ),
         ),
       ),
     );
@@ -94,10 +142,11 @@ class TableRow extends DataTableSource {
     return DataRow.byIndex(
       index: index,
       cells: [
-        DataCell(Text(user.name)),
-        DataCell(Text(user.role)),
+        DataCell(Center(child: Text(user.name))),
+        DataCell(Center(child: Text(user.role))),
         DataCell(
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(icon: Icon(Icons.edit), onPressed: () {}),
               IconButton(icon: Icon(Icons.delete), onPressed: () {}),
