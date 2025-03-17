4259535f1d61bdf060e2ab1be13bd60f8b53fbe7
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-16 19:09:35 -0500

    Se agrega nueva ruta

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomeView(),
         // '/': (context) => const LoginView(),
+        // '/home': (context) => const HomeView(),
       }, 
     );
   }
