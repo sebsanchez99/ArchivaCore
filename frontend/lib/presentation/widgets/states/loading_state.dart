44472b7861829ecdb587d9b0dc34f85f2d1137f9
Evafajardo <31.fajardo@gmail.com>
2025-03-21 15:49:40 -0500

    Se creo widget perzonalizado para estado de carga

---
+++ frontend/lib/presentation/widgets/states/loading_state.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoadingState extends StatefulWidget {
+  const LoadingState({super.key});
+
+  @override
+  State<LoadingState> createState() => _LoadingStateState();
+}
+
+class _LoadingStateState extends State<LoadingState>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RotationTransition(
+        turns: _controller,
+        child: Image.asset('assets/images/logo.png', height: 80, width: 80),
+      ),
+    );
+  }
+}
