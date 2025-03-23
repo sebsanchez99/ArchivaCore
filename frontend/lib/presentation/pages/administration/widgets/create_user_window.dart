f3fa0f281066396455bdce30805e0fb923744fd8
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-22 22:40:07 -0500

    Se agregan nuevos estilos a ventana emergente de creación de usuarios

--- frontend/lib/presentation/pages/administration/widgets/create_user_window.dart
+++ frontend/lib/presentation/pages/administration/widgets/create_user_window.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/presentation/constants/schema_colors.dart';
 import 'package:frontend/presentation/pages/administration/bloc/administration_bloc.dart';
 import 'package:frontend/presentation/pages/administration/bloc/administration_events.dart';
 import 'package:frontend/presentation/pages/administration/bloc/administration_state.dart';
@@ -19,56 +20,65 @@ class CreateUserWindow extends StatelessWidget {
         "Crear Nuevo Usuario",
         style: TextStyle(fontWeight: FontWeight.bold),
       ),
-      content: BlocBuilder<AdministrationBloc, AdministrationState>(
-        bloc: bloc,
-        builder: (context, state) {
-          return state.maybeMap(
-            loaded: (value) {
-              return SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      "Complete el formulario para crear un nuevo usuario en el sistema",
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Nombre de usuario",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10),
-                    CustomInput(
-                      labeltext: "Ingrese el nombre de usuario",
-                      isPassword: false,
-                      onChanged: (text) => bloc.add(ChangeUsernameEvent(username: text.trim())),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Rol del usuario",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    ...value.roles.map((role) => RadioListTile<String>(title: Text(role), value: role, groupValue: value.selectedRole, onChanged: (value) => bloc.add(ChangeRoleEvent(role: value!)),),),
-                    SizedBox(height: 20),
-                    Text(
-                      "Contraseña",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 20),
-                    CustomInput(isPassword: true, labeltext: "Contraseña", onChanged: (text) => bloc.add(ChangePasswordEvent(password: text))),
-                    SizedBox(height: 20),
-                    CustomInput(
-                      isPassword: true,
-
-                      labeltext: "Confirmar contraseña",
-                    ),
-                  ],
-                ),
-              );
-            },
-            orElse: () => LoadingState(),
-          );
-        },
+      content: SizedBox(
+        width: 400,
+        child: BlocBuilder<AdministrationBloc, AdministrationState>(
+          bloc: bloc,
+          builder: (context, state) {
+            return state.maybeMap(
+              loaded: (value) {
+                return SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Complete el formulario para crear un nuevo usuario en el sistema",
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Nombre de usuario",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      CustomInput(
+                        labeltext: "Ingrese el nombre de usuario",
+                        isPassword: false,
+                        onChanged: (text) => bloc.add(ChangeUsernameEvent(username: text.trim())),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Rol del usuario",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      ...value.roles.map((role) => RadioListTile<String>(
+                        title: Text(role), 
+                        value: role, 
+                        dense: true,
+                        activeColor: SchemaColors.neutral700,
+                        groupValue: value.selectedRole, 
+                        onChanged: (value) => bloc.add(ChangeRoleEvent(role: value!)))
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Contraseña",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      CustomInput(isPassword: true, labeltext: "Contraseña", onChanged: (text) => bloc.add(ChangePasswordEvent(password: text))),
+                      SizedBox(height: 20),
+                      CustomInput(
+                        isPassword: true,
+                        labeltext: "Confirmar contraseña",
+                      ),
+                    ],
+                  ),
+                );
+              },
+              orElse: () => LoadingState(),
+            );
+          },
+        ),
       ),
       actions: [
         CustomButton(
@@ -77,13 +87,10 @@ class CreateUserWindow extends StatelessWidget {
         ),
         CustomButton(
           message: 'Crear Usuario',
-          onPressed: () {
+          onPressed: () async {
             Navigator.pop(context);
-            bloc.add(
-              CreateUserEvent(
-                
-              ),
-            );
+            await Future.delayed(Duration(milliseconds: 200));
+            bloc.add(CreateUserEvent());
           },
         ),
       ],
