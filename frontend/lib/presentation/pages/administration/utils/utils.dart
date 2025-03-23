9b7ca96a390757a006072a7061006bf95fdaf564
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-22 22:39:04 -0500

    Se agrega nuevo atributo para evitar que se cierren ventanas emergentes al dar click afuera

--- frontend/lib/presentation/pages/administration/utils/utils.dart
+++ frontend/lib/presentation/pages/administration/utils/utils.dart
@@ -1,29 +1,28 @@
 part of '../view/adminitration_view.dart';
 
 //Muestra el dialogo para editar un usuario
-Future<void> _showEditDialog(BuildContext context) async {
-  return showDialog(context: context, builder: (context) => EdituserWindow());
+Future<void> _showEditDialog(BuildContext context, UserModel user) async {
+  final bloc = context.read<AdministrationBloc>();
+  return showDialog(
+    context: context, 
+    barrierDismissible: false,
+    builder: (context) => EdituserWindow(bloc: bloc,user: user,)
+  );
 }
 
 Future<void> _showCreateDialog(BuildContext context) async {
   final bloc = context.read<AdministrationBloc>();
   return showDialog(
     context: context,
+    barrierDismissible: false,
     builder: (context) => CreateUserWindow(bloc: bloc),
   );
 }
 
-Future<void> _showResult(
-  BuildContext context,
-  ServerResponseModel response,
-) async {
+Future<void> _showResult(BuildContext context, ServerResponseModel response) async {
   return showDialog(
     context: context,
-    builder:
-        (context) =>
-            response.result
-                ? SuccessDialog (message: response.message)
-                : ErrorDialog(message: response.message),
+    builder: (context) => response.result ? SuccessDialog (message: response.message) : ErrorDialog(message: response.message),
   );
 }
 
@@ -32,10 +31,9 @@ Future<void> _showInfoDialog(BuildContext context, String userID) async {
   final bloc = context.read<AdministrationBloc>();
   return showDialog(
     context: context,
-    builder:
-        (context) => InfoDialog(
-          message: '¿Desea eliminar el usuario?',
-          onPressed: () => bloc.add(DeleteUserEvent(userID: userID)),
-        ),
+    builder:(context) => InfoDialog(
+      message: '¿Desea eliminar el usuario?',
+      onPressed: () => bloc.add(DeleteUserEvent(userID: userID)),
+    ),
   );
 }
