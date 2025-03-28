50578445d43c61deb407480653805c5e2e1e2fbb
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-28 11:20:46 -0500

    Se configuran nueva ruta de acceso con sus respectivos hijos

--- landing/src/router/index.ts
+++ landing/src/router/index.ts
@@ -1,14 +1,22 @@
+import NotFound404 from '@/layouts/error/NotFound404.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // Not found
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFound404
+    },
+    // Landing Page
     {
       path: '/',
       name: 'landing',
       component: () => import('@/views/LandingPage.vue'),
     },
     
+    // Auth Page
     {
       path: '/auth',
       component: () => import('@/views/AuthPage.vue'),
@@ -25,7 +33,14 @@ const router = createRouter({
         }
       ]
     },
-
+    
+    // DashboardPage
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('@/views/DashboardPage.vue')
+    }
+    
   ],
 })
 
