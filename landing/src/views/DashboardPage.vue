3a12ae1514b9c525a078ccdfdfa2de28c95c317e
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-28 11:18:09 -0500

    Se agrega vista de dashboard

---
+++ landing/src/views/DashboardPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col">
+        <main>
+            <SideMenuSection/>
+            <RouterView/>
+        </main>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import SideMenuSection from '@/layouts/dashboard/SideMenuSection.vue'
+</script>
