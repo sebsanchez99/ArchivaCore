478041ac8af187a7b8a25895bdf1757dfd496c2c
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-03-29 17:48:26 -0500

    Se implementa un diseño de menú lateral responsivo en la vista de dashboard

--- landing/src/views/DashboardPage.vue
+++ landing/src/views/DashboardPage.vue
@@ -1,12 +1,23 @@
 <template>
-    <div class="flex flex-col">
-        <main>
-            <SideMenuSection/>
-            <RouterView/>
-        </main>
+    <div class="drawer lg:drawer-open">
+        <input id="sidebar" type="checkbox" class="drawer-toggle" />
+
+        <!-- Contenido principal -->
+        <div class="drawer-content bg-white text-black">
+            <label for="sidebar"
+                class="btn btn-primary drawer-button lg:hidden m-2 p-1 bg-transparent border-none shadow-2xl">
+                <Bars3Icon class="w-8 h-8" />
+            </label>
+
+            <RouterView />
+        </div>
+
+        <!-- Sidebar -->
+        <SideMenuSection />
     </div>
 </template>
 
 <script lang="ts" setup>
-import SideMenuSection from '@/layouts/dashboard/SideMenuSection.vue'
+import SideMenuSection from '@/layouts/dashboard/SideMenuSection.vue';
+import { Bars3Icon } from "@heroicons/vue/24/outline";
 </script>
