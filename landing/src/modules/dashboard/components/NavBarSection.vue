9040cd22b7c7ce0012587134c05d30ebee762253
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-04-05 14:35:10 -0500

    Se crea componente navbar de dashboard

---
+++ landing/src/modules/dashboard/components/NavBarSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="navbar bg-primary-500 px-6">
+        <div class="navbar-start">
+            <h1 class="text-xl font-semibold text-primary-100">Panel de Administración</h1>
+        </div>
+        <div class="navbar-end gap-2">
+            <button class="btn btn-outline border border-primary-300 text-primary-100 hover:bg-primary-400 shadow-md">
+                <WalletIcon class="w-4" />
+                <h3>Gestionar Suscripción</h3>
+            </button>
+            <button class="btn btn-ghost btn-circle">
+                <BellIcon class="w-7 text-primary-100" />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { BellIcon, WalletIcon } from "@heroicons/vue/24/outline";
+</script>
