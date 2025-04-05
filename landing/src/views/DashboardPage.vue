17ad7912804040294fb238e300f8e0c57abc70ef
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-04-05 14:35:38 -0500

    Se implementa compomentes navbar a vista de dashboard

--- landing/src/views/DashboardPage.vue
+++ landing/src/views/DashboardPage.vue
@@ -3,13 +3,18 @@
         <input id="sidebar" type="checkbox" class="drawer-toggle" />
 
         <!-- Contenido principal -->
-        <div class="drawer-content bg-white text-black">
-            <label for="sidebar"
-                class="btn btn-primary drawer-button lg:hidden m-2 p-1 bg-transparent border-none shadow-2xl">
-                <Bars3Icon class="w-8 h-8" />
-            </label>
+        <div class="flex flex-col drawer-content bg-white text-black py-0 h-screen">
+            <div class="flex flex-row bg-primary-500 shadow-lg">
+                <label for="sidebar"
+                    class="btn btn-primary drawer-button lg:hidden m-2 p-1 bg-transparent border-none shadow-2xl">
+                    <Bars3Icon class="w-8 h-8" />
+                </label>
+                <NavBarSection />
+            </div>
 
-            <RouterView />
+            <div class="p-5 bg-white">
+                <RouterView />
+            </div>
         </div>
 
         <!-- Sidebar -->
@@ -18,6 +23,7 @@
 </template>
 
 <script lang="ts" setup>
-import SideMenuSection from '@/layouts/dashboard/SideMenuSection.vue';
+import SideMenuSection from '@/modules/dashboard/components/SideMenuSection.vue';
+import NavBarSection from '@/modules/dashboard/components/NavBarSection.vue';
 import { Bars3Icon } from "@heroicons/vue/24/outline";
 </script>
