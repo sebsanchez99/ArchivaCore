499ee132598baa994f999772d92561704e79214d
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-04-26 12:01:52 -0500

    Se agrega funcionalidad a botones de sección principal

--- landing/src/components/buttons/PrimaryButton.vue
+++ landing/src/components/buttons/PrimaryButton.vue
@@ -1,21 +1,31 @@
 <template>
-    <button
-      :class="[
-        'font-semibold px-6 py-3 rounded-lg transition cursor-pointer',
-        primary
-          ? 'bg-primary-500 text-white hover:bg-primary-700'
-          : 'bg-secondary-300 text-white hover:bg-secondary-400'
-      ]"
-      :aria-label="label"
-    >
-      <slot />
-    </button>
-  </template>
-  
-  <script setup>
-  defineProps({
-    primary: Boolean, 
-    label: String, 
-  });
-  </script>
-  
+  <component
+    :is="primary ? RouterLink : 'button'"
+    :to="primary ? '/register' : undefined"
+    @click="!primary && scrollAndClose('pricing')"
+    :class="[
+      'btn btn-lg font-semibold px-6 py-3 rounded-lg transition cursor-pointer border-none',
+      primary
+        ? 'bg-primary-500 text-white hover:bg-primary-700'
+        : 'bg-secondary-300 text-white hover:bg-secondary-400'
+    ]"
+    :aria-label="label"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+defineEmits(['scrollTo']);
+
+const scrollAndClose = (sectionId) => {
+  document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+};
+
+defineProps({
+  primary: Boolean,
+  label: String,
+});
+</script>
