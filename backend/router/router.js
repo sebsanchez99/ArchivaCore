edcb5c212452d75d51798cf880d9ac51f3bed2e2
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-05-05 07:49:41 -0500

    Se añaden nuevas rutas relacionadas con página web

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,15 +1,22 @@
 const { Router } = require('express')
-const passport =require('passport')
-const authRouter = require('../router/auth.router')
-const adminRouter = require('../router/admin.router')
-const checkRole  = require('../middlewares/checkRole')
+const passport = require('passport')
+const authRouter = require('./app/auth.router')
+const adminRouter = require('./app/admin.router')
+const checkRole = require('../middlewares/checkRole')
 
 const router = Router()
 
+// Rutas de aplicación
 // Ruta de autenticación
-router.use('/auth',authRouter)
+router.use('/auth', authRouter)
 // Ruta de administración
-router.use('/admin', passport.authenticate('jwt', {session: false}), checkRole, adminRouter)
+router.use('/admin', passport.authenticate('jwt', { session: false }), checkRole('Administrador', 'Empresa', 'Superusuario'), adminRouter)
+
+// Rutas de página web
+// Ruta de autenticación
+router.use('/web/auth', authRouter)
+// Ruta de administración
+router.use('/web/admin', passport.authenticate('jwt', { session: false }), checkRole('Superusuario'), adminRouter)
 
 module.exports = router
 
