0e99469798baa96f066660a55351141b8feef4e7
Camiloo92 <casallas.ivan@gmail.com>
2025-05-05 21:10:54 -0500

    Se implementan nuevos controladores de página web

--- backend/router/router.js
+++ backend/router/router.js
@@ -1,22 +1,21 @@
 const { Router } = require('express')
 const passport = require('passport')
-const authRouter = require('./app/auth.router')
-const adminRouter = require('./app/admin.router')
+const authAppRouter = require('./app/auth.router')
+const adminAppRouter = require('./app/admin.router')
+const authWebRouter = require('./web/auth.router')
 const checkRole = require('../middlewares/checkRole')
 
 const router = Router()
 
 // Rutas de aplicación
 // Ruta de autenticación
-router.use('/auth', authRouter)
+router.use('/auth', authAppRouter)
 // Ruta de administración
-router.use('/admin', passport.authenticate('jwt', { session: false }), checkRole('Administrador', 'Empresa', 'Superusuario'), adminRouter)
+router.use('/admin', passport.authenticate('jwt', { session: false }), checkRole('Administrador', 'Empresa', 'Superusuario'), adminAppRouter)
 
 // Rutas de página web
 // Ruta de autenticación
-router.use('/web/auth', authRouter)
-// Ruta de administración
-router.use('/web/admin', passport.authenticate('jwt', { session: false }), checkRole('Superusuario'), adminRouter)
+router.use('/web/auth', authWebRouter)
 
 module.exports = router
 
