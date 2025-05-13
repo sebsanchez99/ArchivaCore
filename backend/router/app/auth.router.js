d7553d732bc91d58d780605093b3d9b04f6d7623
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-05-12 22:08:49 -0500

    Se aplica nueva autenticación local a ruta de autenticación de aplicación

--- backend/router/app/auth.router.js
+++ backend/router/app/auth.router.js
@@ -5,6 +5,6 @@ const { login } = require('../../controllers/app/auth.controller.js')
 const router = Router()
 
 // http://localhost:3000/api/v1/auth/login
-router.post('/login', passport.authenticate('local', {session: false}), login)
+router.post('/login', passport.authenticate('appLocalStrategy', {session: false}), login)
 
 module.exports = router 
