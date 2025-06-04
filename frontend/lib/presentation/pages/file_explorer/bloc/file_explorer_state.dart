f8735f4d31174c9b893255e773c8653957eab701
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-04 15:50:59 -0500

    Se agregam atributos a estado de cargado a estado de bloc de vista de explorador de archivos

--- frontend/lib/presentation/pages/file_explorer/bloc/file_explorer_state.dart
+++ frontend/lib/presentation/pages/file_explorer/bloc/file_explorer_state.dart
@@ -1,6 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:frontend/domain/failures/http_request_failure.dart';
+import 'package:frontend/domain/models/folder_model.dart';
 import 'package:frontend/domain/models/server_response_model.dart';
+import 'package:frontend/presentation/global/enums/file_explorer_view_type.dart';
 
 part 'file_explorer_state.freezed.dart';
 
@@ -9,7 +11,12 @@ class FileExplorerState with _$FileExplorerState {
   //Estado de cargando de la vista de archivos
   factory FileExplorerState.loading() = _LoadingState;
   //Estado de cargado de la vista de archivos
-  factory FileExplorerState.loaded() = _LoadedState;
+  factory FileExplorerState.loaded({
+    required FileExplorerViewType viewType,
+    required List<FolderModel> folders,
+    required List<FolderModel> filteredFolders,
+    ServerResponseModel? response,
+  }) = _LoadedState;
   //Estado de falla de la vista de archivos
   factory FileExplorerState.failed(HttpRequestFailure failure) = _FailedState;
 }
