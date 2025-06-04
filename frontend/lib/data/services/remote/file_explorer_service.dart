60fda493e15cfd9a2b1e4404812ba0634c7b18c9
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-04 15:47:33 -0500

    Se crea servicio para vista de explorador de archivos

---
+++ frontend/lib/data/services/remote/file_explorer_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+import 'package:frontend/data/services/handler/dio_client.dart';
+import 'package:frontend/data/services/handler/dio_handler.dart';
+import 'package:frontend/domain/models/server_response_model.dart';
+import 'package:frontend/domain/typedefs.dart';
+
+class FileExplorerService {
+
+  final Dio _dio = DioClient().client;
+
+  HttpFuture<ServerResponseModel> getFolders() {
+    return DioHandler.handleRequest(
+      () => _dio.get(
+        '/supa/listFoldersForUser',
+        data: {
+          'companyName': 'empresa1',
+          'userName': 'usuario1'
+        }
+      ),
+      (response) => ServerResponseModel.fromJson(response),
+    );
+  }
+  
+}
