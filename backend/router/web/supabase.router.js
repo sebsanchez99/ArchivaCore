3af5266693fbaa126fefd588441b29a7e935d7b7
Jaacck19 <186075680+Jaacck19@users.noreply.github.com>
2025-06-10 20:41:29 -0500

    Se corrige comentario de endpoint que calcula almacenamiento total

--- backend/router/web/supabase.router.js
+++ backend/router/web/supabase.router.js
@@ -1,10 +1,9 @@
 const { Router } = require('express')
-const passport = require('passport')
 const { getTotalStorage } = require('../../controllers/web/supabase.controller')
 
 const router = Router()
 
-// http://localhost:3000/api/v1/web/supabase/getTotalStore
+// http://localhost:3000/api/v1/web/supabase/getTotalStorage
 router.get('/getTotalStorage', getTotalStorage)
 
 module.exports = router
