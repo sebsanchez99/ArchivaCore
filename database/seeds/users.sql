30f413a880d61d652048b491c0e444c8b118cc09
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-21 20:57:28 -0500

    Se crean usauarios de prueba usando función

--- database/seeds/users.sql
+++ database/seeds/users.sql
@@ -1,10 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 -- Usuario de prueba
-INSERT INTO usuario (usu_id, usu_nombre, usu_hash, usu_rol)
-VALUES (
-  gen_random_uuid(), 
-  'admin', 
-  crypt('12345', gen_salt('bf', 10)), 
-  4
-);
+SELECT agregar_usuario('admin', 'SuperAdministrador', '12345', 4, NULL);
+SELECT agregar_usuario('Usuario_prueba', 'Usuario Prueba', '12345', 1, 'UUID_DE_LA_EMPRESA'); -- Reemplaza con un UUID válido de una empresa existente
