f7c5e5e3dddc052b52a98c0dd48bedd0ce73874a
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-22 15:59:23 -0500

    Se corrige respuesta de endpoint de autenticación de página web

--- backend/controllers/web/auth.controller.js
+++ backend/controllers/web/auth.controller.js
@@ -2,26 +2,13 @@ const AuthHelper = require('../../helpers/auth.helper')
 const SupaBaseHelper = require('../../helpers/supaBase.helper')
 const ResponseUtil = require('../../utils/response.util')
 
-const register =  async (req, res) => {
-    try {
-        const { companyName, companyEmail, password } = req.body
-        const authHelper = new AuthHelper()
-        const supabaseHelper = new SupaBaseHelper()
-        const result = await authHelper.registerCompany(companyName, companyEmail, password)
-        const bucketResult = await supabaseHelper.createCompanyBucket(companyName)
-        if (!result.success || !bucketResult) {
-            res.status(400).send(ResponseUtil.fail('No se pudo registrar la empresa'))
-        }        
-        res.json(result)
-    } catch (error) {
-        res.status(500).send(ResponseUtil.fail(error.message))
-    }
-}
-
 const login = async (req, res) => {
     try {
         const { _emp_id, _emp_nombre, _emp_activo, _plan_nombre, _plan_duracion  } = req.user
         const authHelper = new AuthHelper()
+        if (!_emp_activo) {
+            return res.json(ResponseUtil.fail('Su cuenta se encuentra inactiva. Contacte con soporte.'))
+        }
         const payload = {
             companyId: _emp_id,
             companyName: _emp_nombre,
@@ -29,12 +16,30 @@ const login = async (req, res) => {
             planName: _plan_nombre,
             planDuration: _plan_duracion
         }
-        const result = authHelper.generateToken(payload)
+        const token = authHelper.generateToken(payload)
+        res.json(ResponseUtil.success('Usuario autenticado con éxito', token))
+    } catch (error) {
+        res.status(500).send(ResponseUtil.fail(error.message))
+    }
+}
+
+
+const register =  async (req, res) => {
+    try {
+        const { companyName, fullname, companyEmail, password } = req.body
+        const authHelper = new AuthHelper()
+        const supabaseHelper = new SupaBaseHelper()
+        const result = await authHelper.registerCompany(companyName, fullname, companyEmail, password)
+        const bucketResult = await supabaseHelper.createCompanyBucket(companyName)
+        if (!bucketResult) {
+            return res.status(400).send(ResponseUtil.fail('No se pudo registrar la empresa'))
+        }           
         res.json(result)
     } catch (error) {
         res.status(500).send(ResponseUtil.fail(error.message))
     }
 }
+
 
 module.exports = {
     register,
