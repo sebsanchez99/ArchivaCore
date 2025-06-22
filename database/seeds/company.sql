040d7741fd0d164fa81bc3f6b3328d151c1735a9
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-22 11:04:01 -0500

    Se hashea contraseña en datos de prueba sql

--- database/seeds/company.sql
+++ database/seeds/company.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- Función para agregar una empresa
-SELECT agregar_empresa('Empresa Prueba S.A.', 'Empresa Prueba Sociedad Anónima', 'contacto@empresaprueba.com', '12345');
+SELECT agregar_empresa('Empresa Prueba S.A.', 'Empresa Prueba Sociedad Anónima', 'contacto@empresaprueba.com', crypt('12345', gen_salt('bf', 10)));
--- database/seeds/users.sql
+++ database/seeds/users.sql
@@ -1,4 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 -- Usuario de prueba
-SELECT agregar_usuario('admin', 'SuperAdministrador', '12345', 4, NULL);
-SELECT agregar_usuario('Usuario_prueba', 'Usuario Prueba', '12345', 1, 'UUID_DE_LA_EMPRESA'); -- Reemplaza con un UUID válido de una empresa existente
+SELECT agregar_usuario('admin', 'SuperAdministrador', crypt('12345', gen_salt('bf', 10)), 4, NULL);
+SELECT agregar_usuario('Usuario_prueba', 'Usuario Prueba', crypt('12345', gen_salt('bf', 10)), 1, 'UUID_DE_LA_EMPRESA'); -- Reemplaza con un UUID válido de una empresa existente
