0d6acfdb08d88f8180698a928b2a63d96506d52c
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-21 20:57:00 -0500

    Se crea compañia de prueba usando función

--- database/seeds/company.sql
+++ database/seeds/company.sql
@@ -1,11 +1,4 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-INSERT INTO Empresa (Emp_ID, Emp_Nombre, Emp_Correo, Emp_Hash, Emp_Plan, Emp_Activo)
-VALUES (
-    uuid_generate_v4(),
-    'Empresa Prueba S.A.',
-    'contacto@empresaprueba.com',
-    crypt('12345', gen_salt('bf', 10)),
-    1,
-    TRUE
-);
+-- Función para agregar una empresa
+SELECT agregar_empresa('Empresa Prueba S.A.', 'Empresa Prueba Sociedad Anónima', 'contacto@empresaprueba.com', '12345');
