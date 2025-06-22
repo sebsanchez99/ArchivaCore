4072cf48d51221c54a25c3ce7dc5e93714602c94
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-22 15:25:43 -0500

    Se aumenta número de usuarios de plan DEMO a 4

--- database/seeds/plans.sql
+++ database/seeds/plans.sql
@@ -1,4 +1,4 @@
 INSERT INTO Plan (Plan_Nombre, Plan_Duracion, Plan_Almacenamiento, Plan_MaxUsuarios, Plan_Precio)
 VALUES
 ('PRO', 365, 0, -1, 1000000), 
-('DEMO', 14, 2147483648, 3, 0);
+('DEMO', 14, 2147483648, 4, 0);
