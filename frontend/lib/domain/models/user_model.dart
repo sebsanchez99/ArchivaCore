cc1ae3790dafd77c3b7a9d1c38588f60b7f9d11d
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-23 17:53:16 -0500

    Se agregan atributos adicionales a modelo de usuario

--- frontend/lib/domain/models/user_model.dart
+++ frontend/lib/domain/models/user_model.dart
@@ -10,10 +10,16 @@ class UserModel with _$UserModel {
   factory UserModel({
     /// Id del usuario
     @JsonKey(name: '_usu_id') required String id,
+    /// Nombre completo de usuario
+    @JsonKey(name: '_usu_nombre_completo') required String fullname,
     /// Nombre del usuario
     @JsonKey(name: '_usu_nombre') required String name,
     /// Rol del usuario
     @JsonKey(name: '_rol_nombre') required String role,
+    // Id de rol de usuario
+    @JsonKey(name: '_rol_id') required int roleId,
+    @JsonKey(name: '_usu_activo') required bool active,
+
   }) = _UserModel;
 
   /// Parsea los datos de la respuesta de tipo JSON y los transforma en un dato de tipo [Map]
