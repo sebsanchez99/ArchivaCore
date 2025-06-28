54aba8f0df4923b3ec9886b4f34ab5051afe8465
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-06-28 18:36:31 -0500

    Se implementar validadores de campos y se refactoriza código de store

--- landing/src/stores/authStore.ts
+++ landing/src/stores/authStore.ts
@@ -10,57 +10,48 @@ export const useAuthStore = defineStore("auth", {
     error: null as string | null,
   }),
   actions: {
-    async registerCompany(payload: { companyName: string; companyEmail: string; password: string }) {
-      this.loading = true;
-      this.error = null;
+    async registerCompany(payload: { companyName: string; fullname: string; companyEmail: string; password: string }) {
       try {
+        this.error = null;
         const response = await authService.registerCompany(payload);
-
-        const data = response.data as ServerResponseModel<null>;
-
+        const data = response.data as ServerResponseModel;
         if (!data.result) {
-          this.error = "No se pudo registrar la empresa";
+          this.error = data.message || "No se pudo registrar la empresa";
+          return data;
         }
-
         return data;
       } catch (error: any) {
+        this.loading = false;
         this.error = "No se pudo registrar la empresa";
         throw error;
-      } finally {
-        this.loading = false;
-      }
+  }
     },
+
     async loginCompany(payload: { companyEmail: string; password: string }) {
-      this.loading = true;
-      this.error = null;
       try {
+        this.error = null;
         const response = await authService.loginCompany(payload);
-        const data = response.data as ServerResponseModel<{ token: string }>;
-
-        if (data.result) {
-          this.token = data.data.token;
-
-          // Guardar el token en localStorage
-          if (this.token) {
-            localStorage.setItem("authToken", this.token);
-          }
-        } else {
-          this.error = "Usuario o contraseña incorrectos";
+        const data = response.data as ServerResponseModel;
+        if (!data.result) {
+          this.error = data.data.message || "Usuario o contraseña incorrectos";
+          return data;
         }
-
+        this.token = data.data;
+        localStorage.setItem("authToken", this.token!);
         return data;
       } catch (error: any) {
-        this.error = error.response?.data?.message || "Usuario o contraseña incorrectos";
-        throw error;
-      } finally {
         this.loading = false;
-      }
+        this.error = "Usuario o contraseña incorrectos";
+        throw error;
+      } 
     },
+
     logout() {
       this.user = null;
       this.token = null;
       localStorage.removeItem("authToken");
     },
+
     clearError() {
       this.error = null;
     },
