fd5a1aadfb4e6c60ed930f11942eacdfcce91dbd
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-07-05 16:32:17 -0500

    Se retornan nuevos datos a petición de inicio de sesión de página web

--- backend/controllers/web/auth.controller.js
+++ backend/controllers/web/auth.controller.js
@@ -4,25 +4,57 @@ const ResponseUtil = require('../../utils/response.util')
 
 const login = async (req, res) => {
     try {
-        const { _emp_id, _emp_nombre, _emp_activo, _plan_nombre, _plan_duracion  } = req.user
         const authHelper = new AuthHelper()
-        if (!_emp_activo) {
-            return res.json(ResponseUtil.fail('Su cuenta se encuentra inactiva. Contacte con soporte.'))
+        const user = req.user
+
+        if (user._emp_id) {
+            if (!user._emp_activo) {
+                return res.json(ResponseUtil.fail('Su cuenta se encuentra inactiva. Contacte con soporte.'))
+            }
+            const planStartDate = new Date(user._emp_fechainicioplan)
+            const payload = {
+                companyId: user._emp_id,
+                companyName: user._emp_nombre,
+                active: user._emp_activo,
+                planName: user._plan_nombre,
+                planDuration: user._plan_duracion,
+                rol: "Empresa"
+            }
+            const token = authHelper.generateToken(payload)
+            return res.json(ResponseUtil.success('Usuario autenticado con éxito', {
+                token,
+                companyId: user._emp_id,
+                companyName: user._emp_nombre,
+                active: user._emp_activo,
+                planName: user._plan_nombre,
+                planDuration: user._plan_duracion,
+                planStartDate: planStartDate,
+                rol: "Empresa"
+            }))
         }
-        const payload = {
-            companyId: _emp_id,
-            companyName: _emp_nombre,
-            active: _emp_activo,
-            planName: _plan_nombre,
-            planDuration: _plan_duracion
+
+        if (user._usu_id && user._rol_nombre) {
+            const payload = {
+                userId: user._usu_id,
+                fullname: user._usu_nombre,
+                email: user._usu_correo,
+                rol: user._rol_nombre
+            }
+            const token = authHelper.generateToken(payload)
+            return res.json(ResponseUtil.success('Usuario autenticado con éxito', {
+                userId: user._usu_id,
+                token,
+                fullname: user._usu_nombre,
+                rol: user._rol_nombre
+            }))
         }
-        const token = authHelper.generateToken(payload)
-        res.json(ResponseUtil.success('Usuario autenticado con éxito', token))
+
+        // Si no es ninguno de los anteriores
+        return res.status(401).json(ResponseUtil.fail('No tiene permisos para acceder'))
     } catch (error) {
         res.status(500).send(ResponseUtil.fail(error.message))
     }
 }
-
 
 const register =  async (req, res) => {
     try {
