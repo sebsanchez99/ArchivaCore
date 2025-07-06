4f5a2c329839912e70de2dff7ea4c1da91d57ff8
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-07-06 16:36:55 -0500

    Se oculta opción para usuarios que notengan rol de empresa

--- landing/src/modules/dashboard/components/NavBarSection.vue
+++ landing/src/modules/dashboard/components/NavBarSection.vue
@@ -1,17 +1,23 @@
 <template>
-    <div class="navbar bg-primary-500 px-6">
-        <div class="navbar-start">
-            <h1 class="text-xl font-semibold text-white">Panel de Administración</h1>
-        </div>
-        <div class="navbar-end gap-2">
-            <button class="btn btn-outline bg-primary-600 border-none text-white hover:bg-primary-700 shadow-md">
-                <WalletIcon class="w-4" />
-                <h3>Gestionar Suscripción</h3>
-            </button>
-        </div>
+  <div class="navbar bg-primary-500 px-6">
+    <div class="navbar-start">
+      <h1 class="text-xl font-semibold text-white">Panel de Administración</h1>
     </div>
+    <div class="navbar-end gap-2">
+      <button
+        v-if="!['Superusuario', 'Soporte'].includes(userRole)"
+        class="btn btn-outline bg-primary-600 border-none text-white hover:bg-primary-700 shadow-md">
+        <WalletIcon class="w-4" />
+        <h3>Gestionar Suscripción</h3>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { WalletIcon } from "@heroicons/vue/24/solid";
+import { useAuthStore } from "@/stores/authStore";
+
+const authStore = useAuthStore();
+const userRole = authStore.getRol;
 </script>
