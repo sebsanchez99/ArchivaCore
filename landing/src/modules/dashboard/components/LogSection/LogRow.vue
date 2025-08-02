ff79f464d6a3f80884eff45cf448c4b9e15c3668
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-02 08:19:27 -0500

    Se crean nuevos componentes para mostrar logs de empresa y logs en general

---
+++ landing/src/modules/dashboard/components/LogSection/LogRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr>
+    <td>{{ log.table }}</td>
+    <td>{{ log.type }}</td>
+    <td>{{ log.description }}</td>
+    <td>{{ log.date }}</td>
+    <td>{{ log.username || 'Desconocido' }}</td>
+    <td>
+      <button class="btn btn-xs bg-primary-500 text-white" @click="$emit('viewDetail', log)">
+        Ver detalle
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script setup lang="ts">
+import type { Log } from '@/interfaces/log'
+defineProps<{ log: Log }>()
+defineEmits(['viewDetail'])
+
+</script>
