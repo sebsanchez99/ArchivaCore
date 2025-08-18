5b10ae778d1f7bf4f52042e393cc824e29e42010
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-18 15:15:23 -0500

    Se inyecta repositorio de análisis de hoja de vida

--- frontend/lib/presentation/global/providers/providers.dart
+++ frontend/lib/presentation/global/providers/providers.dart
@@ -1,12 +1,15 @@
 import 'package:frontend/data/repositories_impl/administration_repository_impl.dart';
+import 'package:frontend/data/repositories_impl/ai_repository_impl.dart';
 import 'package:frontend/data/repositories_impl/auth_repository_impl.dart';
 import 'package:frontend/data/repositories_impl/chat_respository_impl.dart';
 import 'package:frontend/data/repositories_impl/file_explorer_repository_impl.dart';
 import 'package:frontend/data/services/remote/administration_service.dart';
+import 'package:frontend/data/services/remote/ai_service.dart';
 import 'package:frontend/data/services/remote/auth_service.dart';
 import 'package:frontend/data/services/remote/file_explorer_service.dart';
 import 'package:frontend/data/services/socket/chat_socket_service.dart';
 import 'package:frontend/domain/repositories/administration_repository.dart';
+import 'package:frontend/domain/repositories/ai_repository.dart';
 import 'package:frontend/domain/repositories/auth_repository.dart';
 import 'package:frontend/domain/repositories/chat_repository.dart';
 import 'package:frontend/domain/repositories/file_explorer_repository.dart';
@@ -26,6 +29,9 @@ List<SingleChildWidget> appProviders = [
   ),
   Provider<ChatRepository>(
     create: (_) => ChatRepositoryImpl(ChatSocketService()),
+  ),
+  Provider<AIRepository>(
+    create: (_) => AIRepositoryImpl(AIService()),
   ),
 ];
 
