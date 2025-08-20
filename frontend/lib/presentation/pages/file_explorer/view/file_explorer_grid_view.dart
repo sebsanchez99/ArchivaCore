f2c4c018db65b2c2fa49618f7573c37ba6d33fcb
leonardo11012020 <leonardorry767@gmail.com>
2025-08-19 19:25:28 -0500

    Creacion de widget de animacion de bodega

--- frontend/lib/presentation/pages/file_explorer/view/file_explorer_grid_view.dart
+++ frontend/lib/presentation/pages/file_explorer/view/file_explorer_grid_view.dart
@@ -7,9 +7,16 @@ import 'package:frontend/presentation/pages/file_explorer/utils/utils.dart';
 import 'package:frontend/presentation/widgets/folder/custom_folder.dart';
 import 'package:frontend/presentation/widgets/states/loading_state.dart';
 
-class FileExplorerGridView extends StatelessWidget {
+class FileExplorerGridView extends StatefulWidget {
   final FileExplorerBloc bloc;
   const FileExplorerGridView({super.key, required this.bloc});
+
+  @override
+  State<FileExplorerGridView> createState() => _FileExplorerGridViewState();
+}
+
+class _FileExplorerGridViewState extends State<FileExplorerGridView> {
+  dynamic selectedFolder;
 
   @override
   Widget build(BuildContext context) {
@@ -17,53 +24,68 @@ class FileExplorerGridView extends StatelessWidget {
     final width = MediaQuery.of(context).size.width;
 
     return BlocBuilder<FileExplorerBloc, FileExplorerState>(
-      bloc: bloc,
+      bloc: widget.bloc,
       builder: (context, state) {
         return state.maybeMap(
           loaded: (value) {
             final folders = value.filteredFolders;
-            return SizedBox(
-              height: height * 0.5,
-              width: width * 0.9,
-              child: GridView.count(
-                crossAxisCount: 7,
-                crossAxisSpacing: 5,
-                children:
-                    folders.map((folder) {
-                      final totalSize = folder.files.fold<double>(
-                        0.0,
-                        (sum, file) =>
-                            sum + (double.tryParse(file.size) ?? 0.0),
-                      );
-                      return Stack(
-                        children: [
-                          CustomFolder(
-                            onPressed:(){},
-                            icon: Icons.folder,
-                            name: folder.name,
-                            fileCount: folder.files.length.toString(),
-                            size: "${totalSize.toStringAsFixed(2)} MB",
-                          ),
-                          Positioned(
-                            top: 0,
-                            right: 0,
-                            child: PopupMenuButton<String>(
-                              tooltip: 'Opciones',
-                              icon: Icon(Icons.more_horiz, size: 20),
-                              elevation: 7,
-                              color: SchemaColors.background,
-                              onSelected: (String option) {
-                                if (option == 'edit') {
-                                  showEditFolderDialog(context);
-                                  print('Editar carpeta: ${folder.name}');
-                                } else if (option == 'organize') {
-                                  // Acción para organizar
-                                  print('Organizar carpeta: ${folder.name}');
-                                }
-                              },
-                              itemBuilder:
-                                  (BuildContext context) =>
-                                      <PopupMenuEntry<String>>[
+            return Column(
+              children: [
+                SizedBox(
+                  height: height * 0.5,
+                  width: width * 0.9,
+                  child: GridView.count(
+                    crossAxisCount: 7,
+                    crossAxisSpacing: 5,
+                    children:
+                        folders.map((folder) {
+                          final totalSize = folder.files.fold<double>(
+                            0.0,
+                            (sum, file) =>
+                                sum + (double.tryParse(file.size) ?? 0.0),
+                          );
+                          return Stack(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedFolder = folder;
+                                  });
+                                },
+                                child: CustomFolder(
+                                  onPressed: () {
+                                    setState(() {
+                                      selectedFolder = folder;
+                                    });
+                                  },
+                                  icon: Icons.folder,
+                                  name: folder.name,
+                                  fileCount: folder.files.length.toString(),
+                                  size: "${totalSize.toStringAsFixed(2)} MB",
+                                ),
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: PopupMenuButton<String>(
+                                  tooltip: 'Opciones',
+                                  icon: Icon(Icons.more_horiz, size: 20),
+                                  elevation: 7,
+                                  color: SchemaColors.background,
+                                  onSelected: (String option) {
+                                    if (option == 'edit') {
+                                      showEditFolderDialog(context);
+                                      print('Editar carpeta: ${folder.name}');
+                                    } else if (option == 'organize') {
+                                      print(
+                                        'Organizar carpeta: ${folder.name}',
+                                      );
+                                    }
+                                  },
+                                  itemBuilder:
+                                      (
+                                        BuildContext context,
+                                      ) => <PopupMenuEntry<String>>[
                                         PopupMenuItem<String>(
                                           value: 'edit',
                                           child: Row(
@@ -85,12 +107,53 @@ class FileExplorerGridView extends StatelessWidget {
                                           ),
                                         ),
                                       ],
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList(),
+                  ),
+                ),
+                if (selectedFolder != null) ...[
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      height: 110, // Ajusta la altura máxima visible del contenido
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${selectedFolder.name}',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
                             ),
-                          ),
-                        ],
-                      );
-                    }).toList(),
-              ),
+                            const SizedBox(height: 8),
+                            if (selectedFolder.files.isEmpty)
+                                Column(
+                                children: [
+                                  Icon(Icons.folder_off, size: 48, color: const Color.fromARGB(255, 66, 137, 195)),
+                                  SizedBox(height: 8),
+                                  Text('Esta carpeta está vacía.'),
+                                ],
+                              ),
+                            ...selectedFolder.files.map<Widget>(
+                              (file) => ListTile(
+                                leading: Icon(Icons.insert_drive_file),
+                                title: Text(file.name),
+                                subtitle: Text('Tamaño: ${file.size} MB'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ],
             );
           },
           orElse: () => LoadingState(),
