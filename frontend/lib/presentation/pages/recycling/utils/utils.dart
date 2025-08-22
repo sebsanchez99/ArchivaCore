38559026bdf6411bda4b2c171ecd866dc2c4c37f
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-22 16:21:30 -0500

    Se crean utilidades para vista de reciclaje

---
+++ frontend/lib/presentation/pages/recycling/utils/utils.dart
@@ -0,0 +1,61 @@
+part of '../view/recycling_view.dart';
+
+Future<void> _showResult(BuildContext context, ServerResponseModel response) async {
+  return showDialog(
+    context: context,
+    builder: (context) => response.result ? SuccessDialog (message: response.message) : ErrorDialog(message: response.message),
+  );
+}
+
+Future<void> _showFolderDetailsDialog(BuildContext context, FolderModel folder) async {
+  return showDialog(
+    context: context, 
+    barrierDismissible: true,
+    builder: (context) => FolderDetailsWindow(folder: folder),
+  );
+}
+
+Future<void> _showDeleteFileInfoDialog(BuildContext context, FileModel file) async {
+  final bloc =  context.read<RecycleBloc>();
+  return showDialog(
+    context: context, 
+    builder: (context) => InfoDialog(
+      message: '¿Desea eliminar definitivamente el archivo ${file.name}?. Esta acción es irreversible.', 
+      onPressed: () => bloc.add(DeleteFileEvent(filePath: file.path)),
+    ),
+  );
+}
+
+Future<void> _showDeleteFolderInfoDialog(BuildContext context, FolderModel folder) async {
+  final bloc =  context.read<RecycleBloc>();
+  return showDialog(
+    context: context, 
+    builder: (context) => InfoDialog(
+      message: '¿Desea eliminar definitivamente la carpeta ${folder.name}?. Esta acción es irreversible.', 
+      onPressed: () => bloc.add(DeleteFolderEvent(folderPath: folder.path)),
+    ),
+  );
+}
+
+Future<void> _showRestoreFileInfoDialog(BuildContext context, FileModel file) async {
+  final bloc =  context.read<RecycleBloc>();
+  return showDialog(
+    context: context, 
+    builder: (context) => InfoDialog(
+      message: '¿Desea restaurar el archivo ${file.name}?', 
+      onPressed: () => bloc.add(RestoreFileEvent(filePath: file.path)),
+    ),
+  );
+}
+
+Future<void> _showRestoreFolderInfoDialog(BuildContext context, FolderModel folder) async {
+  final bloc =  context.read<RecycleBloc>();
+  return showDialog(
+    context: context, 
+    builder: (context) => InfoDialog(
+      message: '¿Desea restaurar el archivo ${folder.name}?', 
+      onPressed: () => bloc.add(RestoreFolderEvent(folderPath: folder.path)),
+    ),
+  );
+}
+
