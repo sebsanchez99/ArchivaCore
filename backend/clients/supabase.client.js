b1b1927b67e3889176a9c41d3fddd82bb2f8ca83
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-21 20:25:49 -0500

    Se crea cliente se supabase para realizar operaciones directas

---
+++ backend/clients/supabase.client.js
@@ -0,0 +1,15 @@
+const poolNewClient = require('../libs/supaBase');
+
+const SupabaseClient = {
+  createBucket: (bucketName, options) => poolNewClient.createBucket(bucketName, options),
+  deleteBucket: (bucketName) => poolNewClient.deleteBucket(bucketName),
+  emptyBucket: (bucketName) => poolNewClient.emptyBucket(bucketName),
+  upload: (bucket, filePath, fileBuffer, options) => poolNewClient.from(bucket).upload(filePath, fileBuffer, options),
+  download: (bucket, filePath) => poolNewClient.from(bucket).download(filePath),
+  list: (bucket, path, options) => poolNewClient.from(bucket).list(path, options),
+  copy: (bucket, fromPath, toPath) => poolNewClient.from(bucket).copy(fromPath, toPath),
+  remove: (bucket, filePaths) => poolNewClient.from(bucket).remove(filePaths),
+  move: (bucket, fromPath, toPath) => poolNewClient.from(bucket).move(fromPath, toPath),
+};
+
+module.exports = SupabaseClient;
