bf68450a0396b65ed4283f850b444c41f332086c
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-25 16:04:31 -0500

    Se modifica bloc de vista de notificaciones

--- frontend/lib/presentation/pages/notification/bloc/notification_events.dart
+++ frontend/lib/presentation/pages/notification/bloc/notification_events.dart
@@ -1,11 +1,19 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:frontend/presentation/enums/notification_filter.dart';
 
 part 'notification_events.freezed.dart';
 
 @freezed
 class NotificationEvents with _$NotificationEvents {
   factory NotificationEvents.initialize() = InititalizeEvent;
+  factory NotificationEvents.silentInitialize() = SilentInititalizeEvent;
+  factory NotificationEvents.filter({ required NotificationFilter filter }) = FilterEvent;
   factory NotificationEvents.deleteNotification({ required String notificationId }) = DeleteNotificationEvent;
   factory NotificationEvents.deleteAllNotifications() = DeleteAllNotificationsEvent;
   factory NotificationEvents.markNotification({ required String notificationId }) = MarkNotificationEvent;
+  factory NotificationEvents.markAllNotifications() = MarkAllNotificationEvent;
+  factory NotificationEvents.createNotification({ 
+    required String title,
+    required String message,
+  }) = CreateNotificationEvent;
 }
