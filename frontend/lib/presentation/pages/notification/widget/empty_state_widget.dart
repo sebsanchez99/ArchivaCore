da36a84aecae6753421a5963b6b72f6602afad63
Sebastian Sánchez <122845344+sebsanchez99@users.noreply.github.com>
2025-08-25 16:04:09 -0500

    Se crea widget personalizado para notificaciones vacías

---
+++ frontend/lib/presentation/pages/notification/widget/empty_state_widget.dart
@@ -0,0 +1,50 @@
+// lib/presentation/widgets/empty_state_widget.dart
+
+import 'package:flutter/material.dart';
+
+class EmptyStateWidget extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const EmptyStateWidget({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 80,
+            color: Colors.grey[400],
+          ),
+          const SizedBox(height: 16),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.grey[700],
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            subtitle,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
